d8dc610916350210798482cda8d37b1e70c95e09
= <mywork.anon@protonmail.com>
2025-01-10 14:27:48 -0300

    test

--- src/edu/fuscaopreto/ContaPoupanca.java
+++ src/edu/fuscaopreto/ContaPoupanca.java
@@ -8,7 +8,7 @@ public class ContaPoupanca extends Conta {
 
 	@Override
 	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Poupança ===");
+		System.out.println("=== Extrato Conta Poupança ===-");
 		super.imprimirInfosComuns();
 	}
 }
